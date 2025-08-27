fb33cab254d7a6f3b13740c48d89fa5015c9a54b
unknown <ahmadalnujaidi02@gmail.com>
2025-08-27 08:06:29 +0300

    user input complete

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+//    public static void main(String[] args){
+//        System.out.println("Hello World");
+//    } INTRO
+
+//    public static void main(String[] args){
+//        int x = 5;
+//        double y = 10.50;
+//        char symbol = 'A';
+//        String name = "Ahmad Alnujaidi";
+//        boolean isStudent = false;
+//        System.out.println(name + ' '+ y);
+//    } VARIABLES
+//    public static void main(String[] args){
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("enter your name: ");
+//        String name = scanner.nextLine(); // entire line
+////        String name = scanner.next() //  next doesnt read after whitespace
+//        System.out.print("Enter Age: ");
+//        int age = scanner.nextInt();
+//
+//        System.out.print("enter gpa: ");
+//        double gpa = scanner.nextDouble();
+//
+//        System.out.print("are you student (true/false): ");
+//        boolean isStudent = scanner.nextBoolean();
+//
+//        System.out.println("Hello "+name + " you are age: "+age+" with gpa of: "+gpa);
+//
+//        if (isStudent){
+//            System.out.println("you are student");
+//        } else {
+//            System.out.println("you are NOT student");
+//        }
+//
+//        scanner.close();
+//    } USER INPUT
+}
