a1dddffd5eb646603133888a5c46a3f621c93f98
unknown <ahmadalnujaidi02@gmail.com>
2025-09-03 14:25:02 +0300

    printf

--- src/Main.java
+++ src/Main.java
@@ -160,4 +160,41 @@ public class Main {
 //        System.out.print("hypotenuse: "+c);
 //        scanner.close();
 //    } MATH CLASS
+
+//    public static void main(String[] args){
+//        // printF format
+//         // %[flags][width][.precision][specifier-character]
+////        String name = "Spongebob";
+////        char firstLetter = 'S';
+////        int age = 30;
+////        double height = 60.5;
+////        boolean isEmployed = true;
+////
+////        System.out.printf("Hello %s\n", name);
+////        System.out.printf("Your name starts with a %c\n", firstLetter);
+////        System.out.printf("your age is %d years old\n", age);
+////        System.out.printf("You are %f cm tall\n", height);
+////        System.out.printf("employed: %b\n", isEmployed);
+////
+////        System.out.printf("%s is %d years old",name,age);
+//
+////        double price1 = 9.99;
+////        double price2 = 100.15;
+////        double price3 = -54.01;
+////
+////        System.out.printf("%.1f\n", price1);
+////        System.out.printf("%.2f\n", price2);
+////        System.out.printf("%.1f\n", price3);
+//
+//        int id1 = 1;
+//        int id2 = 23;
+//        int id3 = 456;
+//        int id4 = 7890;
+//
+//        System.out.printf("%4d\n", id1);
+//        System.out.printf("%4d\n", id2);
+//        System.out.printf("%4d\n", id3);
+//        System.out.printf("%4d\n", id4);
+//
+//    } // PRINTF
 }
