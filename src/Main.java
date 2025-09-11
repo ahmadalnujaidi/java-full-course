77be8aadd228ef984664de8ff4f55738c4bdf7a4
unknown <ahmadalnujaidi02@gmail.com>
2025-09-11 12:45:28 +0300

    selection sort

--- src/Main.java
+++ src/Main.java
@@ -516,6 +516,31 @@ public class Main {
 //            }
 //        }
 //    } BUBBLE SORT
+//    public static void main(String[] args){
+//        int array[] = {8,7,9,2,3,1,5,4,6};
+//
+////        for(int i: array){
+////            System.out.println(i);
+////        }
+//
+//        selectionSort(array);
+//        for (int i : array){
+//            System.out.println(i);
+//        }
+//    }
+//    static void selectionSort(int[] array){
+//        for(int i =0; i < array.length - 1; i++){
+//            int min = i;
+//            for(int j = i + 1; j < array.length; j++){
+//                if(array[min] > array[j]){
+//                    min = j;
+//                }
+//            }
+//            int temp = array[i];
+//            array[i] = array[min];
+//            array[min] = temp;
+//        } // SELECTION SORT
+    }
 }
 
 
