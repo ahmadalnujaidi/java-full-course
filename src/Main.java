1a69b3c2749bc06d58de874d422e6cff542b3bbf
unknown <ahmadalnujaidi02@gmail.com>
2025-09-15 08:52:14 +0300

    insertion sort

--- src/Main.java
+++ src/Main.java
@@ -540,7 +540,30 @@ public class Main {
 //            array[i] = array[min];
 //            array[min] = temp;
 //        } // SELECTION SORT
-    }
+//    public static void main(String[] args){
+//        int array[] = {9,1,8,2,7,3,6,5,4};
+//        for (int i : array){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//        insertionSort(array);
+//        for (int i : array){
+//            System.out.print(i + " ");
+//        }
+//    } // INSERTION SORT
+//
+//    private static void insertionSort(int[] array) {
+//        for(int i = 1; i < array.length; i++){
+//            int temp = array[i];
+//            int j = i - 1;
+//
+//            while(j>=0 && array[j] > temp){
+//                array[j + 1] = array[j];
+//                j--;
+//            }
+//            array[j + 1] = temp;
+//        }
+//  }
 }
 
 
