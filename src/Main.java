a8833d0710e6b71860026a00014320712ad4c025
unknown <ahmadalnujaidi02@gmail.com>
2025-09-25 13:43:10 +0300

    merge sort

--- src/Main.java
+++ src/Main.java
@@ -591,6 +591,71 @@ public class Main {
 //        }
 //        return x * factorial(x - 1);
 //    } // RECURSION
+
+//    public static void main(String[] args){
+//        int[] array = {8,2,5,3,4,7,6,1};
+//
+//        for(int i: array){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//        mergeSort(array);
+//        for(int i: array){
+//            System.out.print(i + " ");
+//        }
+//    }
+//    public static void mergeSort(int[] array){
+//        int length = array.length;
+//        if (length <= 1) return; // base case
+//        int middle = length / 2;
+//        int[] leftArray = new int[middle];
+//        int[] rightArray = new int[length - middle];
+//
+//        int i = 0; // left array
+//        int j = 0; // right array
+//
+//        for(;i < length; i++){
+//            if(i < middle){
+//                leftArray[i] = array[i];
+//            }
+//            else {
+//                rightArray[j] = array[i];
+//                j++;
+//            }
+//        }
+//        mergeSort(leftArray);
+//        mergeSort(rightArray);
+//        merge(leftArray, rightArray, array);
+//    }
+//    public static void merge(int[] leftArray, int[] rightArray, int[] array){
+//        int leftSize = leftArray.length;
+//        int rightSize = rightArray.length;
+//        int i = 0, l = 0, r = 0;
+//
+//        // check conditions
+//        while(l < leftSize && r < rightSize){
+//            if(leftArray[l] < rightArray[r]){
+//                array[i] = leftArray[l];
+//                i++;
+//                l++;
+//            }
+//            else {
+//                array[i] = rightArray[r];
+//                i++;
+//                r++;
+//            }
+//        }
+//        while(l < leftSize){
+//            array[i] = leftArray[l];
+//            i++;
+//            l++;
+//        }
+//        while(r < rightSize){
+//            array[i] = rightArray[r];
+//            i++;
+//            r++;
+//        }
+//    } // MERGE SORT
 }
 
 
