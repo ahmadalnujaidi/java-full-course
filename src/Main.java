0d75e5887939f871d74328c46f8233c2ab995df5
unknown <ahmadalnujaidi02@gmail.com>
2025-10-01 08:42:21 +0300

    quick sort

--- src/Main.java
+++ src/Main.java
@@ -656,6 +656,44 @@ public class Main {
 //            r++;
 //        }
 //    } // MERGE SORT
+//    public static void main(String[] args){
+//        int[] array = {6,3,1,5,7, 2, 9, 8, 4};
+//        for(int i: array){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//        quickSort(array, 0, array.length - 1);
+//        for(int i: array){
+//            System.out.print(i + " ");
+//        }
+//    }
+//    private static void quickSort(int[] arr, int start, int end){
+//        if(end <= start){ // base case
+//            return;
+//        }
+//
+//        int pivot = partition(arr, start, end);
+//        quickSort(arr, start, pivot - 1);
+//        quickSort(arr, pivot + 1, end);
+//    }
+//    private static int partition(int[] arr, int start, int end){
+//        int pivot = arr[end];
+//        int i  = start - 1;
+//        for(int j = start; j < end; j++){
+//            if(arr[j] < pivot){
+//                i++;
+//                int temp = arr[i];
+//                arr[i] = arr[j];
+//                arr[j] = temp;
+//            }
+//        }
+//        i++;
+//        int temp = arr[i];
+//        arr[i] = arr[end];
+//        arr[end] = temp;
+//
+//        return i;
+//    } // QUICK SORT
 }
 
 
